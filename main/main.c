a6fb57f383b9cfa39a64c9bb75c16605c390a447
Fsmall06 <Fsmall06@users.noreply.github.com>
2026-05-30 20:54:40 +0800

    Save current project state

--- main/main.c
+++ main/main.c
@@ -2,20 +2,27 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "st7789_min_test.h"
+#include "env.h"
 
 static const char *TAG = "APP_MAIN";
 
 void app_main(void)
 {
-    ESP_LOGI(TAG, "ST7789 minimal init trace start");
+    ESP_LOGI(TAG, "APP start");
 
-    ESP_ERROR_CHECK(st7789_min_init());
-
-    ESP_LOGI(TAG, "ST7789 minimal init trace done");
+    esp_err_t ret = env_init();
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "env_init failed: %s", esp_err_to_name(ret));
+    }
+    else
+    {
+        ESP_LOGI(TAG, "ENV started");
+    }
 
     while (1)
     {
+        ESP_LOGI(TAG, "alive");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
